058c2bff841fee2db6cf9ea48f05aff2f00f676a
Mandell1981 <mandell316@gmail.com>
2024-05-08 16:35:12 -0400

    Check

--- app.py
+++ app.py
@@ -1,15 +1,2 @@
-# config                    
-from flask import Flask
-app = Flask(__name__)
-
-
-#index route
-@app.route('/')
-def index():
-    return 'Hello, this is PetFax!'
-
-
-
-@app.route('/pets')
-def pets():
-    return 'This is a list of pets available for adoption'
+from petfax import create_app
+app = create_app()
---
+++ petfax/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask 
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello():
+        return 'Hello, this is PetFax!'
+    
+    # register pet blueprint
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    # register user blueprint
+    
+    return app
